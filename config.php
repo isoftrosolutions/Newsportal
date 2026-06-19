4b6f5c2f6244b800f4254dc8a79ccffea7c6fb28
PAtel18751 <mind59024@gmail.com>
2026-06-19 15:21:05 +0545

    Remove trailing slash from SITE_URL to prevent double-slash URLs

--- config.php
+++ config.php
@@ -6,7 +6,7 @@ define('DB_NAME', 'gtnewzco_gtnews');
 
 define('SITE_NAME', 'न्यूज पोर्टल');
 define('SITE_TAGLINE', 'सत्य समाचार, सही जानकारी');
-define('SITE_URL', 'https://www.gtnewz.com/');
+define('SITE_URL', 'https://www.gtnewz.com');
 define('UPLOAD_PATH', __DIR__ . '/uploads/');
 define('UPLOAD_URL', SITE_URL . '/uploads/');
 
